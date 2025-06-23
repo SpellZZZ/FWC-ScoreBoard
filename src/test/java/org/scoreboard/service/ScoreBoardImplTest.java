c559a685d588ec999f4a97b32162e6e79c38366b
Jakub Brzezinski <james321321@wp.pl>
2025-06-23 14:57:22 +0200

    ADD test #1 (Red)

--- src/test/java/org/scoreboard/service/ScoreBoardImplTest.java
+++ src/test/java/org/scoreboard/service/ScoreBoardImplTest.java
@@ -1,8 +1,12 @@
 package org.scoreboard.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.scoreboard.model.Match;
 import org.scoreboard.repo.MatchRepository;
 import org.scoreboard.repo.MatchRepositoryImpl;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +21,27 @@ class ScoreBoardImplTest {
         scoreBoard = new ScoreBoardImpl(matchRepository);
     }
 
+    @Test
+    void startGame() {
+        //given
+        String homeTeam = "Mexico";
+        String awayTeam = "Canada";
+        int expectedListSize = 1;
+        int homeTeamExpectedScore = 0;
+        int awayTeamExpectedScore = 0;
+
+        //when
+        scoreBoard.startGame(homeTeam, awayTeam);
+
+        //then
+        List<Match> summary = matchRepository.findAll();
+        assertEquals(expectedListSize, summary.size());
+
+        Match match = summary.getFirst();
+        assertEquals(homeTeam, match.getHomeTeam());
+        assertEquals(awayTeam, match.getAwayTeam());
+        assertEquals(homeTeamExpectedScore, match.getHomeScore());
+        assertEquals(awayTeamExpectedScore, match.getAwayScore());
+    }
 
 }
