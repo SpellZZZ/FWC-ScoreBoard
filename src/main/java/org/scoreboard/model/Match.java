f883e19f1606275241db20f1c9299092e4881441
Jakub Brzezinski <james321321@wp.pl>
2025-06-23 14:48:34 +0200

    ADD model class

---
+++ src/main/java/org/scoreboard/model/Match.java
@@ -0,0 +1,11 @@
+package org.scoreboard.model;
+
+import java.time.LocalDateTime;
+
+public class Match {
+    private String homeTeam;
+    private String awayTeam;
+    private int homeScore;
+    private int awayScore;
+    private LocalDateTime startTime;
+}
